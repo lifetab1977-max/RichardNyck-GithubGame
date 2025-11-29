032a148e87fe9b154f474ee4fdd0ab40a998b715
NyPtun <lifetab1977@gmail.com>
2025-11-29 06:16:07 +0100

    Player and Map
    Hab wurm gemacht der sich bewegen und drehen kann wie auch eine kleine map

---
+++ scripts/player_wormy.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+
+@onready var animatedsprite = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+
+
+
+	# jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# input direction and movement
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction > 0:
+		animatedsprite.flip_h = false
+	elif direction < 0:
+		animatedsprite.flip_h = true 
+	
+	move_and_slide()
